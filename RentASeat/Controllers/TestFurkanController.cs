762bce21b2a89812d3ba299ed649768bccb996ba
Furkan Erciyas <furkannerciyass@outlook.com>
2024-06-13 21:00:22 +0300

    test furkan

---
+++ RentASeat/Controllers/TestFurkanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentASeat.Controllers
+{
+    public class TestFurkanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
