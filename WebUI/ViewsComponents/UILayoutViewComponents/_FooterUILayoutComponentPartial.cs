d8b4c10138c4d47a1c007d1887dc7414b973642e
furkangul29 <gulfurkan@outlook.com.tr>
2024-06-26 11:40:01 +0300

    .

--- WebUI/ViewsComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ WebUI/ViewsComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DtoLayer.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7250/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
